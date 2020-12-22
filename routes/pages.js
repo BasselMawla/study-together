ccbd8f2fc7a773a9162607bcfa55a070682cb7d9
Bassel Al Mawla <basselalmawla@gmail.com>
2020-12-22 13:14:20 +0200

    Set up DB locally

--- routes/pages.js
+++ routes/pages.js
@@ -5,8 +5,6 @@ const presController = require('../controllers/presentation');
 const messController = require('../controllers/messaging');
 const router = express.Router();
 
-var formidable = require('formidable');
-
 router.get('/', authController.isLoggedIn , (req , res) => {
   res.render('index', {
     user: req.user
