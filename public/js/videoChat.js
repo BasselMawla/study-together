e57c15de63e590ee18204e21eeced206116b438c
Bassel Al Mawla <basselalmawla@gmail.com>
2020-12-23 22:35:15 +0200

    Update videoChat.js

--- public/js/videoChat.js
+++ public/js/videoChat.js
@@ -3,7 +3,7 @@ const videoGrid = document.getElementById("video-grid")
 const myPeer = new Peer(userId, {
     secure: true,
     host: "https://aubstudysawa.herokuapp.com",
-    port: "443"
+    port: "3001"
 })
 const myVideo = document.createElement("video")
 // Mute own audio so the user doesn't hear himself
