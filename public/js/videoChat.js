3c9c536fc659508b0636c3f0ee963e364a16fcde
Bassel Al Mawla <basselalmawla@gmail.com>
2020-12-24 00:11:06 +0200

    Update videoChat.js

--- public/js/videoChat.js
+++ public/js/videoChat.js
@@ -1,7 +1,7 @@
 const socket = io("/")
 const videoGrid = document.getElementById("video-grid")
 const myPeer = new Peer(userId, {
-    host: "/aubstudysawapeerjs.herokuapp.com",
+    host: "/peerjsaubstudy.herokuapp.com",
     port: "3001"
 })
 const myVideo = document.createElement("video")
