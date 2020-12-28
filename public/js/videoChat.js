ea19ec298ce9ab27ea4a23ca00614839b3d6bb73
Bassel Al Mawla <basselalmawla@gmail.com>
2020-12-28 19:48:42 +0200

    peerjs

--- public/js/videoChat.js
+++ public/js/videoChat.js
@@ -2,9 +2,9 @@
 var muted = false;
 const videoGrid = document.getElementById("video-grid")
 const myPeer = new Peer(userId, {
-    host: "peerjs-server.herokuapp.com",
-    secure: true,
-    port: 443
+    host: "/",
+    port: 9000,
+    path: "/peerjs"
 })
 const myVideo = document.createElement("video")
 // Mute own audio so the user doesn't hear himself
