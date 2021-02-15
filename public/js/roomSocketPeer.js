5bb3937f0ea3825424d6d629658ef865e6e7e0af
Bassel Al Mawla <basselalmawla@gmail.com>
2021-02-15 20:12:10 +0200

    Update roomSocketPeer.js

--- public/js/roomSocketPeer.js
+++ public/js/roomSocketPeer.js
@@ -79,7 +79,7 @@ $(document).ready(async function() {
     console.log ("Connecting to peerId: " + data.peerId);
     //peerCount++;
     // When the connection is established
-    conn.on("open", function() {
+    conn.on("open", async function() {
       await conn.send(peerId);
     });
     
